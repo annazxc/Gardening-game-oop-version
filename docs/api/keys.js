8acc8c1f28b1ba2512ccfe8cb8dd2db2febcb8b1
annazxc <88202955+annazxc@users.noreply.github.com>
2025-06-01 16:15:07 +0800

    add keys temporarily

---
+++ docs/api/keys.js
@@ -0,0 +1,9 @@
+export function getApiKey(para) {
+  const keys = {
+    location: "almItGXNHLbw0yOJk7N3",
+    poem: "AIzaSyAHj_woIM0XZWcugorfck5z4nSUgsI7Y-o",
+    gemini: "AIzaSyAHj_woIM0XZWcugorfck5z4nSUgsI7Y-o",
+    groq: "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",
+  };
+  return keys[para];
+}
